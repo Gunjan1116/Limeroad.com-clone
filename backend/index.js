0f0d3895471f687913019a98fd82abd1b6a58a0f
Gunjan1116 <kumargunjan1116@gmail.com>
2023-01-18 10:17:21 +0530

    created connection

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const cors=require("cors");
+const { connection } = require("./config/db");
+require("dotenv").config();
+
+const app=express();
+
+app.use(cors({
+    origin:"*"
+}))
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.json(`Welcome to home page of Light Fashion`)
+})
+
+app.listen(process.env.port,async()=>{
+    try {
+        await connection
+        console.log(`Connected to DB`);
+    } catch (error) {
+        console.log(`Not able to connect to DB`);
+        console.log(error.message);
+    }
+    console.log(`server is running at port ${process.env.port}`)
+})
