605b0273cad075223ac227f87df7259c8558ec7e
Gunjan1116 <kumargunjan1116@gmail.com>
2023-01-21 21:04:01 +0530

    added cart page route

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ const {userRoute}=require("./routes/user.route");
 const {homeproductRoute}=require("./routes/homeLanding.route");
 const {mensRoute}=require("./routes/mens.route");
 const {womensRoute}=require("./routes/womens.route");
+const {cartRoute}=require("./routes/cart.route");
+const {authentication}=require("./middlewares/authentication.middleware");
 require("dotenv").config();
 
 const app=express();
@@ -21,7 +23,8 @@ app.use("/homepageproduct",homeproductRoute);
 app.use("/mens",mensRoute);
 app.use("/womens",womensRoute);
 app.use("/users",userRoute);
-
+app.use(authentication);
+app.use("/cart",cartRoute);
 app.listen(process.env.port,async()=>{
     try {
         await connection
